9aa875c95fbe14acff34caffbed2d5c4519a3dca
Ricardo Alvarez <ralvarezcm@gmail.com>
2021-09-30 11:38:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./src/routes/index');
+const usersRouter = require('./src/routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const myCustomMiddleware = (req, res, next) => {
+    console.log('myCustomMiddleware', req.query);
+    console.log('Hey, Im in the middleware');
+}
+
+app.use('/', myCustomMiddleware, indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
